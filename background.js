7beb763949532811f72faf9dde08a3fd4c7ef88b
Volkan Akin <Volkan.Akin@holidaycheck.com>
2021-07-13 17:48:55 +0200

    add extraHeaders param

--- background.js
+++ background.js
@@ -1,21 +1,32 @@
 chrome.webRequest.onHeadersReceived.addListener(
   function (details) {
-    if (details.initiator.indexOf('dashboard.lablab.app') > -1) {
+    if (
+      details.initiator.indexOf('dashboard.lablab.app') > -1 ||
+      details.initiator.indexOf('localhost') > -1
+    ) {
       for (var j = 0; j < details.responseHeaders.length; ++j) {
         if (
-          details.responseHeaders[j].name.toLowerCase() == 'x-frame-options'
+          details.responseHeaders[j].name.toLowerCase() === 'x-frame-options' ||
+          details.responseHeaders[j].name.toLowerCase() ===
+            'content-security-policy'
         ) {
           details.responseHeaders.splice(j, 1);
-          return {
-            responseHeaders: details.responseHeaders,
-          };
         }
+        // if (details.responseHeaders[j].value.indexOf('frame-ancestors') > -1) {
+        //   const pattern = /(^|;)[\s]*frame-ancestors[\s][^;]*/i;
+        //   details.responseHeaders[j].value = details.responseHeaders[
+        //     j
+        //   ].value.replace(pattern, '; ');
+        // }
       }
+      return {
+        responseHeaders: details.responseHeaders,
+      };
     }
   },
   {
     urls: ['<all_urls>'],
     types: ['sub_frame'],
   },
-  ['blocking', 'responseHeaders']
+  ['blocking', 'extraHeaders', 'responseHeaders']
 );
