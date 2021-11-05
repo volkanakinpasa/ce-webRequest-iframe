f778b95a4262c902da1953e465c11161c235c392
volkanakinpasa <volkanakintr@gmail.com>
2021-11-05 18:04:39 +0100

    update icon & update names

--- background.js
+++ background.js
@@ -1,23 +1,17 @@
 chrome.webRequest.onHeadersReceived.addListener(
   function (details) {
     if (
-      details.initiator.indexOf('dashboard.lablab.app') > -1 ||
-      details.initiator.indexOf('localhost') > -1
+      details.initiator.indexOf("dashboard.lablab.app") > -1 ||
+      details.initiator.indexOf("localhost") > -1
     ) {
       for (var j = 0; j < details.responseHeaders.length; ++j) {
         if (
-          details.responseHeaders[j].name.toLowerCase() === 'x-frame-options' ||
+          details.responseHeaders[j].name.toLowerCase() === "x-frame-options" ||
           details.responseHeaders[j].name.toLowerCase() ===
-            'content-security-policy'
+            "content-security-policy"
         ) {
           details.responseHeaders.splice(j, 1);
         }
-        // if (details.responseHeaders[j].value.indexOf('frame-ancestors') > -1) {
-        //   const pattern = /(^|;)[\s]*frame-ancestors[\s][^;]*/i;
-        //   details.responseHeaders[j].value = details.responseHeaders[
-        //     j
-        //   ].value.replace(pattern, '; ');
-        // }
       }
       return {
         responseHeaders: details.responseHeaders,
@@ -25,8 +19,8 @@ chrome.webRequest.onHeadersReceived.addListener(
     }
   },
   {
-    urls: ['<all_urls>'],
-    types: ['sub_frame'],
+    urls: ["<all_urls>"],
+    types: ["sub_frame"],
   },
-  ['blocking', 'extraHeaders', 'responseHeaders']
+  ["blocking", "extraHeaders", "responseHeaders"]
 );
